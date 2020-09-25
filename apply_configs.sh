83cfd7477773e8611e2dbb973ebb63a48ec9320d
Tim Lane <tilne@amazon.com>
2020-09-25 12:26:30 -0700

    Modify apply_configs.sh as suggested by shellcheck

--- apply_configs.sh
+++ apply_configs.sh
@@ -5,8 +5,8 @@ set -ex
 # vim
 cp -r vim ~/.vim
 cp vimrc ~/.vimrc
-VUNDLE_DIR=${HOME}/.vim/bundle/Vundle.vim
-[ -d ${VUNDLE_DIR} ] || git clone https://github.com/VundleVim/Vundle.vim.git ${VUNDLE_DIR}
+VUNDLE_DIR="${HOME}"/.vim/bundle/Vundle.vim
+[ -d "${VUNDLE_DIR}" ] || git clone https://github.com/VundleVim/Vundle.vim.git "${VUNDLE_DIR}"
 
 # aliases
 cp ./aliases ~/.aliases
@@ -16,22 +16,22 @@ cp bashrc ~/.bashrc
 cp bash_profile ~/.bash_profile
 
 # zsh
-OH_MY_ZSH_HOME=${HOME}/.oh-my-zsh
-[ -d ${OH_MY_ZSH_HOME} ] || git clone https://github.com/ohmyzsh/ohmyzsh ${OH_MY_ZSH_HOME}
+OH_MY_ZSH_HOME="${HOME}"/.oh-my-zsh
+[ -d "${OH_MY_ZSH_HOME}" ] || git clone https://github.com/ohmyzsh/ohmyzsh "${OH_MY_ZSH_HOME}"
 cp zshrc ~/.zshrc
 
 # python
-PYENV_ROOT=${HOME}/.pyenv
-VIRTUALENV_PLUGIN_DIR=${PYENV_ROOT}/plugins/pyenv-virtualenv
-[ -d ${PYENV_ROOT} ] || git clone https://github.com/pyenv/pyenv ${PYENV_ROOT}
-[ -d ${VIRTUALENV_PLUGIN_DIR} ] || git clone https://github.com/pyenv/pyenv-virtualenv.git ${VIRTUALENV_PLUGIN_DIR}
+PYENV_ROOT="${HOME}"/.pyenv
+VIRTUALENV_PLUGIN_DIR="${PYENV_ROOT}"/plugins/pyenv-virtualenv
+[ -d "${PYENV_ROOT}" ] || git clone https://github.com/pyenv/pyenv "${PYENV_ROOT}"
+[ -d "${VIRTUALENV_PLUGIN_DIR}" ] || git clone https://github.com/pyenv/pyenv-virtualenv.git "${VIRTUALENV_PLUGIN_DIR}"
 cp pythonrc ~/.pythonrc
 
 # emacs
 cp ./emacs ~/.emacs
-EMACS_DIR=${HOME}/.emacs.d
-ESHELL_DIR=${EMACS_DIR}/eshell
-cp ./eshell-profile ${ESHELL_DIR}/profile
+EMACS_DIR="${HOME}"/.emacs.d
+ESHELL_DIR="${EMACS_DIR}"/eshell
+cp ./eshell-profile "${ESHELL_DIR}"/profile
 
 # tmux
 cp ./tmux.conf ~/.tmux.conf

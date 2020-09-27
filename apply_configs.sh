187f56de49f1d5398a992231b997ae1b0e1be484
Tim Lane <tilne@amazon.com>
2020-09-27 11:49:27 -0700

    Enable use of clobber option in entrypoint script

--- apply_configs.sh
+++ apply_configs.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
 set -e
-./install-pyenv.sh
-./apply_configs.py
+
+CLOBBER=false
+
+syntax() {
+  cat <<- EOF
+apply_configs.sh [-c]
+  -c/--clobber    Remove and replace files that already exist
+EOF
+}
+
+
+parse_args() {
+  while [ $# -gt 0 ] ; do
+    case "$1" in
+      --clobber|-c)
+        CLOBBER=true
+        ;;
+      -h|--help|help)
+        syntax
+        exit 0
+        ;;
+      *)
+        echo >&2 "Unrecognized option '$1'"
+        syntax
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+
+
+main() {
+  parse_args "$@"
+  CLOBBER_FLAG=""
+  [ "${CLOBBER}" == "true" ] && CLOBBER_FLAG="--clobber"
+  ./install_pyenv.sh ${CLOBBER_FLAG}
+  ./apply_configs.py ${CLOBBER_FLAG}
+}
+
+main "$@"

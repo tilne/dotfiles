b2a9c97edbe3cf597fd89cfdc1b9425670e55d14
Tim Lane <timlane1992@gmail.com>
2022-10-05 14:56:58 -0500

    Install requirements before running python script

--- apply_configs.sh
+++ apply_configs.sh
@@ -38,6 +38,7 @@ main() {
   [ "${CLOBBER}" == "true" ] && CLOBBER_FLAG="--clobber"
   ./install_pyenv.sh ${CLOBBER_FLAG}
   ./install_nvm.sh
+  pip install -r requirements.txt
   ./apply_configs.py ${CLOBBER_FLAG}
 }
 

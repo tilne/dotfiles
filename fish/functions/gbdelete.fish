9f9c439679ea7c113a01614e1c354665162edff2
Tim Lane <lanetim@chrobinson.com>
2022-12-29 15:07:07 -0600

    Avoid deleting main and master branches

--- fish/functions/gbdelete.fish
+++ fish/functions/gbdelete.fish
@@ -1,3 +1,3 @@
 function gbdelete --wraps=gb\ \|\ grep\ -vE\ \'\\smain\(\\s\|\$\)\'\ \|\ xargs\ git\ branch\ -D --description alias\ gbdelete=gb\ \|\ grep\ -vE\ \'\\smain\(\\s\|\$\)\'\ \|\ xargs\ git\ branch\ -D
-  gb | grep -vE '\smain(\s|$)' | xargs git branch -D $argv; 
+  gb | grep -vE '\s(main|master)(\s|$)' | xargs git branch -D $argv;
 end

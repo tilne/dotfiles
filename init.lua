ff7fb40601466e22577c2fb878af30a0e9a4b869
Tim Lane <lanetim@chrobinson.com>
2023-03-16 13:09:43 -0500

    Add keymap for git files

--- init.lua
+++ init.lua
@@ -84,6 +84,7 @@ require'lspconfig'.pylsp.setup{
 -- Telescope keymaps
 local telescope_builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>fF', telescope_builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
 -- TODO: enable this after looking more into use of help_tags
